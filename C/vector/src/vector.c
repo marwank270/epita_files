c5df4b1216292e119bfa34cabc5fdb83d7da0bb6
marwank270 <marwan.kaouachi@gmail.com>
2023-03-23 13:54:29 +0000

    Comments added

--- C/vector/src/vector.c
+++ C/vector/src/vector.c
@@ -3,13 +3,12 @@
 
 typedef struct s_vector
 {
-    void *data_array;
-    size_t sizeof_data;
-    size_t array_capacity;
-    size_t data_count;
+    void *data_array;       // Pointer to a storage space in the memory
+    size_t sizeof_data;     // Size of one element handled by the vector/array
+    size_t array_capacity;  // Actual capacity of the vector/array
+    size_t data_count;      // Number of elements in the vector/array
 }; typedef struct s_vector t_vector;
 
-t_vector *_efvector_new(size_t sizeof_data, size_t initial_capacity)
-{
-    
-}
+t_vector *_efvector_new(size_t sizeof_data, size_t initial_capacity);
+
+#define efvector_new(type, initial_capacity)
